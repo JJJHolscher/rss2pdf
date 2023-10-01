2298934866f890e52f1eb4987714b008e3bd7303
JJJHolscher <jochem.holscher@gmail.com>
2023-10-01 14:32:44 +0200

    option not to merge files

--- src/__main__.py
+++ src/__main__.py
@@ -16,71 +16,117 @@ from argtoml import parse_args
 from pypdf import PdfMerger
 
 
-def get_title(rss):
+def string_for_os(string):
+    def safe_char(c):
+        return c.isalnim() or c in {".", "-", "_", " "}
+
+    return "".join(c for c in string if safe_char(c))
+
+
+def get_feed_title(rss):
+    title = ""
     if "title" in rss.feed:
-        return rss.feed.title
+        title = rss.feed.title
     else:
-        return urlparse(rss.feed.link).netloc
+        title = urlparse(rss.feed.link).netloc
+    return string_for_os(title)
 
 
-def update(url: str, dir: Path, archive_dir: Path, temp_pdfs: Path, new: bool = False):
+def get_chapter_title(rss_entry):
+    title = ""
+    if "title" in rss_entry:
+        title = rss_entry.title
+    else:
+        title = urlparse(rss_entry.link).path
+    return string_for_os(title)
+
+
+def update(url: str, folder: Path, archive_dir: Path, new: bool = False):
     rss = feedparser.parse(url)
-    title = get_title(rss)
-    pdf_path = dir / f"{title}.pdf"
-    archive_path = archive_dir / f"{title}.txt"
-    merger = PdfMerger()
+    feed_title = get_feed_title(rss)
+    archive_path = archive_dir / f"{feed_title}.txt"
+    folder = folder / feed_title
 
     # Read the archive file.
-    if pdf_path.exists():
+    if folder.exists():
         if not archive_path.exists():
-            return print(f"{pdf_path} found but not {archive_path}, skipping")
-        print("updating", pdf_path)
+            return print(f"{folder} found but not {archive_path}, skipping")
+        print("updating", folder)
         with open(archive_path) as f:
             archive = {line.strip() for line in f.readlines()}
-        merger.append(pdf_path)
     elif not new:
-        return print(f'No pdf found at "{pdf_path}". Specify --new to make a new one')
+        return print(f'No dir found at "{folder}". Specify new to make a new one')
     else:
-        print("creating", pdf_path)
+        print("creating", folder)
         archive_dir.mkdir(parents=True, exist_ok=True)
-        pdf_path.parent.mkdir(parents=True, exist_ok=True)
+        folder.mkdir(parents=True, exist_ok=True)
         archive = set()
 
-    # Create the tempororary pdf directory if it doesn't already exist.
-    temp_pdfs.mkdir(parents=True, exist_ok=True)
-
-    # Add every rss entry not already in the pdf to the back of the pdf.
-    entries = list(reversed(rss.entries))
+    # Download every rss entry into their own pdf.
+    entries = list(rss.entries)
     updated = False
-    for i, entry in enumerate(entries):
-        link = entry.link
-        if link in archive:
-            continue
-        updated = True
-        print(f"[{i+1}/{len(entries)}] {link}")
+    with open(archive_path, "a") as archive_file:
+        for i, entry in enumerate(entries):
+            link = entry.link
+            if link in archive:
+                continue
+            updated = True
 
-        temp_pdf = str(temp_pdfs / f"{i}.pdf")
-        pdfkit.from_url(link, temp_pdf)
-        merger.append(temp_pdf)
-        archive.add(link)
+            chapter_title = get_chapter_title(entry)
+            pdf_path = folder / f"{chapter_title}.pdf"
+            print(f"[{i+1}/{len(entries)}] {chapter_title}\t{link}")
+            pdfkit.from_url(link, pdf_path)
+            archive_file.write(link + "\n")
 
-    if updated:
-        # Write the pdf and archive file.
-        print("writing ", pdf_path)
-        merger.write(pdf_path)
-        with open(archive_path, "w") as f:
-            f.write("\n".join(archive))
-        # clean the temporary pdf directory
-        for pdf in temp_pdfs.iterdir():
-            pdf.unlink()
+    return updated
 
 
-def download(urls: List[str], out_dir: Path, archive: Path, temp_dir: Path, new: bool):
+def update_merge(
+    url: str, folder: Path, archive_dir: Path, tmp_folder: Path, new: bool = False
+):
+    feed_title = get_feed_title(feedparser.parse(url))
+    pdf_path = folder / f"{feed_title}.pdf"
+    if not pdf_path.exists() and not new:
+        return print(f'No pdf found at "{pdf_path}". Specify new to make a new one')
+
+    tmp_folder = tmp_folder / feed_title
+    tmp_folder.mkdir(parents=True, exist_ok=True)
+
+    update(url, tmp_folder.parent, archive_dir, new=True)
+    if len(list(tmp_folder.iterdir())) == 0:
+        return False
+
+    # Merge all pdfs found in the temporary folder into one.
+    merger = PdfMerger()
+    if pdf_path.exists():
+        merger.append(pdf_path)
+
+    for pdf in tmp_folder.iterdir():
+        merger.append(pdf)
+    print(f"Merging all downloaded pdfs to {pdf_path}.")
+    merger.write(pdf_path)
+
+    for pdf in tmp_folder.iterdir():
+        pdf.unlink()
+    return True
+
+
+def download(
+    urls: List[str],
+    out_dir: Path,
+    archive: Path,
+    temp_dir: Path,
+    new: bool,
+    merge=False,
+):
     if len(urls) == 0:
         print("no rss urls to check")
 
     for url in urls:
-        update(url, out_dir, archive, temp_dir, new)
+        if merge:
+            update_merge(url, out_dir, archive, temp_dir, new)
+        else:
+            update(url, out_dir, archive, new)
 
 
 if __name__ == "__main__":
@@ -90,5 +136,10 @@ if __name__ == "__main__":
         debugpy.wait_for_client()
 
     download(
-        ARGS.rss.urls, ARGS.out.dir, ARGS.out.archive, ARGS.out.temp_pdfs, ARGS.new
+        ARGS.rss.urls,
+        ARGS.out.dir,
+        ARGS.out.archive,
+        ARGS.out.temp_pdfs,
+        ARGS.new,
+        ARGS.merge,
     )
